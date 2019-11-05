9daeca63d8a80ebb1f3c4aa65050916e949dc2a3
jules <j.vanassche@etu.unistra.fr>
2019-11-05 11:54:16 +0100

    level + icon summoner

--- src/commands/LeagueOfLegends/summoner.js
+++ src/commands/LeagueOfLegends/summoner.js
@@ -5,6 +5,17 @@ require('dotenv').config();
 let baseUrlSummonerId = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 let baseUrlSummonerData = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
 
+let baseUrlddragonCdn = "http://ddragon.leagueoflegends.com/cdn/";
+
+
+let ddragonRiotCdn = {
+    version: "9.21.1",
+    image: {
+        profileicon: "/img/profileicon/",
+        extension: "png"
+    }
+};
+
 const lol_summoner = new Command('lol:summoner', null, null);
 
 lol_summoner.description = " ```Markdown \n **Usage:** " + Command.PREFIX + " lol summoner [summonerName]";
@@ -17,12 +28,7 @@ lol_summoner.action = function (bot, message, args) {
 
         request(urlSummonerId, function (error, response, body) {
             if (response && response.statusCode == 200) {
-
-                let urlSummonerData = baseUrlSummonerData + JSON.parse(body).id +"?api_key=" + process.env.RIOT_GAMES_TOKEN;
-                request(urlSummonerData, function (errror, response, body) {
-                    console.log(body);
-                });
-
+                requestSummonderData(JSON.parse(body), bot, message);
             } else {
                 console.log(error, response.statusCode);
                 return null;
@@ -32,10 +38,31 @@ lol_summoner.action = function (bot, message, args) {
 
 };
 
+function requestSummonderData(summonerInfo, bot, message)
+{
+    let urlSummonerData = baseUrlSummonerData + summonerInfo.id +"?api_key=" + process.env.RIOT_GAMES_TOKEN;
+    request(urlSummonerData, function (errror, response, body) {
+        formatMessageSummoner(JSON.parse(body), summonerInfo, bot, message);
+    });
+}
+
+function formatMessageSummoner(summonerDataRank, summonerInfo, bot, message)
+{
+    let profileIcon = baseUrlddragonCdn + ddragonRiotCdn.version + ddragonRiotCdn.image.profileicon + summonerInfo.profileIconId + "." + ddragonRiotCdn.image.extension;
+
+    console.log(summonerDataRank);
+    message.channel.send("Level: " + summonerInfo.summonerLevel, {
+        embed: {
+            thumbnail: {
+                url: profileIcon,
+                height: 50,
+                width: 50
+            }
+        }
+    });
+
+
+}
+
 
 module.exports = lol_summoner;
-
-// let urlSummonerData = baseUrlSummonerData + bodySumId.id + "?api_key=" + process.env.RIOT_GAMES_TOKEN;
-// request(urlSummonerData, function (error, response, bodySumData) {
-//     console.log(bodySumData);
-// })
