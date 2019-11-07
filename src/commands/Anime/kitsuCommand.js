78b5a35f0af3a03d3c5ca37ddab766ef65394ed1
jules <j.vanassche@etu.unistra.fr>
2019-11-07 22:18:16 +0100

    slugify + search by name kitsu command

--- src/commands/Anime/kitsuCommand.js
+++ src/commands/Anime/kitsuCommand.js
@@ -7,40 +7,65 @@ require('dotenv').config();
 const characterCount = 105772;
 const animeCount = 14705;
 const urlPrefix = "https://kitsu.io/api/edge/";
+const pageLimit = "20";
 
 const commandName = "kitsu";
 const kitsuCommand = new Command(commandName, null, null);
 
 kitsuCommand.embedDescription = new Discord.RichEmbed()
     .setTitle(commandName)
-    .setDescription("Random anime or manga character | Random anime")
-    .addField("Usage", Command.PREFIX + " " + commandName + " anime")
+    .setDescription("Anime information, if no option supplied then it is randomly selected | Random character description")
+    .addField("Usage", Command.PREFIX + " " + commandName + " anime [my anime name]")
     .addField("Usage", Command.PREFIX + " " + commandName + " character");
 
 kitsuCommand.action = function (bot, message, args) {
 
     if (args[0] && args[0] === "anime") {
-        requestRandomAnime(message, bot);
+        if (args.length > 1) {
+            requestSpecificAnime(message, bot, args.slice(1).join(' '));
+        } else {
+            requestRandomAnime(message, bot);
+        }
     } else if (args[0] && args[0] === "character") {
         requestRandomCharacter(message, bot);
     } else {
-        message.channel.send(this.embedDescription)
+        message.channel.send(this.embedDescription);
         return;
     }
 };
+
+function requestSpecificAnime(message, bot, name)
+{
+    let urlGetAnimeByName = urlPrefix + "anime/?filter[slug]=" + Command.slugify(name);
+
+    request(urlGetAnimeByName, (error, response, body) => {
+        if (response && response.statusCode === 200 && body) {
+           let arrayResponse = JSON.parse(body);
+            console.log(arrayResponse.meta.count);
+           //single result , we can send the anime data alone
+           if (arrayResponse.meta.count === 1) {
+               message.channel.send(createEmbedKitsuAnimeMessage(arrayResponse.data[0]));
+           } else {
+               message.channel.send("no result found :cry: \n");
+               message.channel.send(kitsuCommand.embedDescription);
+               return;
+           }
+        }
+    })
+}
 
 
 function requestRandomAnime(message, bot)
 {
     let randomAnimeID = (Math.round(Math.random() * animeCount) + 1);
-    let urlGetAnime = urlPrefix + "anime/" + randomAnimeID;
+    let urlGetRandomAnime = urlPrefix + "anime/" + randomAnimeID;
 
-    request(urlGetAnime, function (error, response, body) {
+    request(urlGetRandomAnime, function (error, response, body) {
         if (response && response.statusCode === 200 && body) {
             let arrayResponse = JSON.parse(body);
             message.channel.send(createEmbedKitsuAnimeMessage(arrayResponse.data));
         } else {
-            console.log(error, response);
+
             if (response.statusCode == 404) {
                 message.channel.send("please retry");
             }
@@ -52,9 +77,9 @@ function requestRandomAnime(message, bot)
 function requestRandomCharacter(message, bot)
 {
     let randomCharacterID = (Math.round(Math.random() * characterCount) + 1);
-    let urlGetCharacterID = urlPrefix + "characters/" + randomCharacterID;
+    let urlGetRandomCharacter = urlPrefix + "characters/" + randomCharacterID;
 
-    request(urlGetCharacterID, function (error, response, body) {
+    request(urlGetRandomCharacter, function (error, response, body) {
         if (response && response.statusCode === 200 && body) {
             let arrayResponse = JSON.parse(body);
             message.channel.send(createEmbedKitsuCharacterMessage(arrayResponse.data));
@@ -68,6 +93,7 @@ function requestRandomCharacter(message, bot)
 function createEmbedKitsuAnimeMessage(anime)
 {
     let animeAttributes = anime.attributes;
+
     return new Discord.RichEmbed()
         .setTitle( (animeAttributes.titles.en_jp || " ") + " | " + (animeAttributes.titles.ja_jp || " "))
         .setDescription(animeAttributes.synopsis || " ")
